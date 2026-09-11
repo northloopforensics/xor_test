f72f5577ef1ce10b37bf33170c2962e56dca0427
northloopforensics <73806121+northloopforensics@users.noreply.github.com>
2026-09-11 09:49:01 -0500

    Update xor_playground.py

--- xor_playground.py
+++ xor_playground.py
@@ -29,7 +29,7 @@ from xor_core import xor_encode, get_ttl_date, xor_validate
 try:
     _KEY_PRESET_A: int = int(st.secrets["XOR_KEY_PRESET_A"], 16)
     _PHRASE_PRESET_B: str = st.secrets["XOR_PHRASE_PRESET_B"]
-    _PHRASE_PRESET_C: str = st.secrest["XOR_PHRASE_PRESET_C"]
+    _PHRASE_PRESET_C: str = st.secrets["XOR_PHRASE_PRESET_C"]
     _APP_PASSWORD: str = st.secrets["APP_PASSWORD"]
 except KeyError as _e:
     st.error(
